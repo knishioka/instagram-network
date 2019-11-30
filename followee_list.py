ffb4f70307a8b492f968b1b39015ebb2ce5f6956
Kenichiro Nishioka <nishioka@3idea.jp>
2019-11-30 11:15:02 +0900

    Add function to get logged in context

--- followee_list.py
+++ followee_list.py
@@ -1,8 +1,34 @@
 import argparse
+import instaloader
+import os
+from functools import lru_cache
 
 
 def main(*, user_ids):
     print(user_ids)
+
+
+@lru_cache(maxsize=None)
+def loader_context():
+    """Get logged in context.
+
+    Returns:
+        InstaloaderContext: logged in context.
+
+    Examples:
+        >>> loader_context()
+        <instaloader.instaloadercontext.InstaloaderContext at 0x111456e10>
+    """
+
+    loader = instaloader.Instaloader()
+    loader.login(os.environ['INSTAGRAM_USER_ID'],
+                 os.environ['INSTAGRAM_PASSWORD'])
+    return loader.context
+
+
+def followees(user_id):
+    profile = instaloader.Profile.from_username(loader_context(), user_id)
+    return [followee.username for followee in profile.get_followees()]
 
 
 if __name__ == '__main__':
