572130d8bd6f7d20e2d7fa77324c73522f9900e2
Kenichiro Nishioka <nishioka@3idea.jp>
2019-11-30 15:26:22 +0900

    Export followee list as json

--- followee_list.py
+++ followee_list.py
@@ -1,11 +1,14 @@
 import argparse
 import instaloader
+import json
 import os
+import sys
 from functools import lru_cache
 
 
 def main(*, user_ids):
-    print(user_ids)
+    all_followees = [{user_id: followees(user_id)} for user_id in user_ids]
+    print(json.dumps({'followees': all_followees}))
 
 
 @lru_cache(maxsize=None)
@@ -27,8 +30,26 @@ def loader_context():
 
 
 def followees(user_id):
+    """Get followees.
+
+    Args:
+        user_id (str): Instagram user_id.
+
+    Returns:
+        list of str: followee id list.
+
+    Examples:
+        >>> followees(target_user_id)
+        ['followee1', 'followee2', 'followee3']
+    """
+    print(f"Getting {user_id}'s followees.", file=sys.stderr)
     profile = instaloader.Profile.from_username(loader_context(), user_id)
-    return [followee.username for followee in profile.get_followees()]
+    followee_list = [followee.username for followee in profile.get_followees()]
+    output = f'{user_id}_followees.json'
+    with open(output, 'w') as io:
+        print(f"Writing {user_id}'s followees to {output}.", file=sys.stderr)
+        json.dump({user_id: followee_list}, io)
+    return followee_list
 
 
 if __name__ == '__main__':
