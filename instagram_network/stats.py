9179a6182ba7dd490df1c47f3df809c272304d27
Kenichiro Nishioka <nishioka@3idea.jp>
2019-12-01 21:45:07 +0900

    Add stats to get followee and follower count

---
+++ instagram_network/stats.py
@@ -0,0 +1,21 @@
+import instaloader
+from instagram_network.loader import context
+
+
+def follower_followee_count(user_id):
+    """Get follower and followee count.
+
+    Args:
+        user_id (str): instagram user_id.
+
+    Returns:
+        dict: follower count and followee count.
+
+    Examples:
+        >>>follower_followee_count('shinzoabe')
+        {'follower_cnt': 487315, 'followee_cnt': 22}
+    """
+
+    profile = instaloader.Profile.from_username(context(), user_id)
+    return {'follower_cnt': profile.followers,
+            'followee_cnt': profile.followees}
