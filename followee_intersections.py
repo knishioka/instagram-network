79c1b7409cf0bb39bdcfdebeb75f083b55b0d9af
Kenichiro Nishioka <nishioka@3idea.jp>
2019-12-01 12:39:18 +0900

    Add followee_intesrsections.py

---
+++ followee_intersections.py
@@ -0,0 +1,29 @@
+import glob
+import json
+import pandas as pd
+from functools import reduce
+
+
+def main():
+    files = glob.glob('*_*.json')
+    targets = [json.load(open(file)) for file in files]
+    all_followee_list = reduce(lambda x, y: {**x, **y}, targets, dict())
+    df = pd.DataFrame({k: list_to_true_value_dict(v)
+                       for k, v in all_followee_list.items()}).fillna(False)
+    df.sum(axis=1).sort_values(ascending=False).to_csv('intersections.csv')
+
+
+def list_to_true_value_dict(user_list):
+    """Convert list to dict with True.
+    The dict is used to initialize Pandas Dataframe.
+
+    Examples:
+        >>> list_to_relation_dict(['a', 'b', 'c'])
+        {'a': True, 'b': True, 'c': True}
+    """
+
+    return {user: True for user in user_list}
+
+
+if __name__ == '__main__':
+    main()
