85fafc0452bbf90b214a56b8b1e4b15fb7fd6ffd
Kenichiro Nishioka <nishioka@3idea.jp>
2019-12-07 10:49:34 +0900

    Get json formatted user profile

---
+++ instagram_network/user.py
@@ -0,0 +1,39 @@
+import argparse
+import instaloader
+import json
+from .loader import context
+
+
+def get_profile(user_id):
+    """Get user profile as json format
+
+    Args:
+        user_id (str): Instagram user_id.
+
+    Returns:
+        str: json formatted user profile
+
+    Examples:
+        >>> get_profile('target_username')
+        '{"userid": 123456, "username": "target_username",...}'
+    """
+    profile = instaloader.Profile.from_username(context(), user_id)
+    return json.dumps({
+        'userid': profile.userid,
+        'username': profile.username,
+        'full_nam': profile.full_name,
+        'is_verified': profile.is_verified,
+        'biography': profile.biography,
+        'followees': profile.followees,
+        'followers': profile.followers,
+        'mediacount': profile.mediacount
+    })
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Get a user profile')
+    parser.add_argument('-u', '--user',
+                        required=True,
+                        help='instagram user ids')
+    args = parser.parse_args()
+    print(get_profile(args.user_id))
