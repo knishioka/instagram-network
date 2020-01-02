a80faafbd16a7653f8a58fcca9f03e679dc34b84
Kenichiro Nishioka <nishioka@3idea.jp>
2020-01-02 10:39:21 +0900

    Add person struct

--- instagram_network/api_server/firestore_crud.go
+++ instagram_network/api_server/firestore_crud.go
@@ -12,7 +12,27 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+type Person struct {
+	Age          int       `json:"age"`
+	Biography    string    `json:"biography""`
+	Born         string    `json:"born"`
+	Followees    int       `json:"followees"`
+	Followers    int       `json:"followers"`
+	Intersection int       `json:"intersection"`
+	IsArtist     bool      `json:"is_artist"`
+	IsVerified   bool      `json:"is_verified"`
+	MediaCount   int       `json:"media_count"`
+	PriceAverage float64   `json:price_average`
+	StoredAt     time.Time `json:"stored_at"`
+	UserID       string    `json:"userid""`
+}
+
+func NewPerson(biography string) *Person {
+	return &Person{Biography: biography}
+}
 
 func main() {
 	e := echo.New()
@@ -35,7 +55,7 @@ func firebaseInit() *firebase.App {
 	return app
 }
 
-func users() {
+func users(num int) {
 	ctx := context.Background()
 	app := firebaseInit()
 	client, err := app.Firestore(ctx)
@@ -46,20 +66,27 @@ func users() {
 	iter := client.Collection("users").Documents(ctx)
 	for {
 		doc, err := iter.Next()
-		if err == iterator.Done {
+		if err == iterator.Done || num == 2 {
 			break
 		}
 		if err != nil {
 			log.Fatalf("Failed to iterate: %v", err)
 		}
 		bytes, _ := json.Marshal(doc.Data())
-		fmt.Println(string(bytes)) // TODO return JSON
+		fmt.Println(doc.Data())
+		var person Person
+		json.Unmarshal(bytes, &person)
+		fmt.Println(string(bytes)) // TODO: return JSON
+		fmt.Println("document id:", doc.Ref.ID)
+		fmt.Println("user_id: ", person.UserID)
+		fmt.Println("media_count", person.MediaCount)
+		num += 1
 	}
 }
 
 func usersHandler() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		users() // TODO get JSON
+		users(0) // TODO: get JSON
 		return c.String(http.StatusOK, "aaa")
 	}
 }
