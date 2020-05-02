985f8ce4680622ac59ef1b08f9d6e41f25712d70
Kenichiro Nishioka <nishioka@3idea.jp>
2020-05-02 12:15:24 +0900

    Enable part update

--- instagram_network/models/base_model.py
+++ instagram_network/models/base_model.py
@@ -83,7 +83,9 @@ class BaseModel(metaclass=ABCMeta):
             BaseModel
 
         """
-        valid_attrs = {k: attrs.get(k) for k in self.valid_keys}
+        current_attrs = self.to_dict()
+        new_attrs = {k: attrs.get(k) for k in self.valid_keys}
+        valid_attrs = {**current_attrs, **new_attrs}
         self.document(self.doc_id).set(valid_attrs)
         for k in self.valid_keys:
             setattr(self, k, valid_attrs.get(k))
